c8b3ae2728b5c025b401410c34ac1fcaf454ee8e
Annika <annikyet@icloud.com>
2022-06-02 16:39:55 -0600

    sync

--- app/Controllers/GiftsController.js
+++ app/Controllers/GiftsController.js
@@ -3,7 +3,7 @@ import { giftsService } from "../Services/GiftsService.js";
 export class GiftsController {
   
   selectGif(id) {
-    console.log('giftsController.selectGif(id)')
+    console.log(`giftsController.selectGif(${id})`)
   }
 
   create() {
