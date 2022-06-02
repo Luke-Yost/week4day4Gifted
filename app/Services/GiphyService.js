a2c7c3c1ff7390dceaec3592798d8434646e6a87
Annika <annikyet@icloud.com>
2022-06-02 15:28:51 -0600

    DOTJS!!!!

--- app/Services/GiphyService.js
+++ app/Services/GiphyService.js
@@ -1,5 +1,5 @@
-import { ProxyState } from "../AppState"
-import { Giphy } from "../Models/Giphy"
+import { ProxyState } from "../AppState.js"
+import { Giphy } from "../Models/Giphy.js"
 
 
 
@@ -11,8 +11,6 @@ const giphyApi = axios.create({
   baseURL: `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&limit=${limit}&rating=${rating}&q=`,
   timeout: 2000
 })
-
-// https://api.giphy.com/v1/gifs/search?api_key=HlU3G9Lxkvf5FQAbZ16iSsGWRcExi0sE&limit=20&q=
 
 
 class GiphyService {
