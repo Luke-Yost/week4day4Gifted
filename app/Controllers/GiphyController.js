5221f8073c2a92c3eec30b44a2a8f12b86a6ac80
Luke Yost <lukexyost@gmail.com>
2022-06-02 16:38:45 -0600

    giphy done

--- app/Controllers/GiphyController.js
+++ app/Controllers/GiphyController.js
@@ -1,12 +1,23 @@
 import { giphyService } from "../Services/GiphyService.js"
+import { ProxyState } from "../AppState.js" 
 
 
-
+function _drawGiphys(){
+  let template = ''
+  ProxyState.giphys.forEach(g => template += g.Template)
+  document.getElementById('gifs').innerHTML = template
+}
 export class GiphyController {
+
+  constructor(){
+    ProxyState.on('giphys', _drawGiphys)
+  }
 
   search() {
     window.event.preventDefault()
     let search = window.event.target.search.value
     giphyService.search(search)
   }
+
+
 }
