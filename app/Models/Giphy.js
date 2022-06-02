c0d164d42801b55fa3fcf64aed1a9f78e357a6de
Luke Yost <lukexyost@gmail.com>
2022-06-02 16:25:29 -0600

    added model things

--- app/Models/Giphy.js
+++ app/Models/Giphy.js
@@ -1,10 +1,17 @@
+import { generateId } from "../Utils/generateId.js"
+
 export class Giphy {
   constructor(data) {
     this.gifUrl = data.images.original.webp
+    this.id = data.id || generateId()
     console.log(this.gifUrl)
   }
 
   get Template() {
-    return this.gifUrl
+    return `
+    <div id="${this.id} class="card m-3" style="width: 18rem;">
+      <img onclick="app.giftsController.selectGif()" src="${this.gifUrl}" class="card-img-top" alt="gif">
+  </div>
+    `
   }
 }
